788502fbc317dba4eb35d76cc7d60107ba2dcf8d
ceejayps <ceejayps1308@gmail.com>
2022-04-23 21:15:30 -0500

    Create auth-state.dart

---
+++ lib/container/auth-state.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class authState extends StatefulWidget {
+  const authState({Key? key}) : super(key: key);
+
+  @override
+  State<authState> createState() => _authStateState();
+}
+
+class _authStateState extends State<authState> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
