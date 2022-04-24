614dc143f745800906b2131827ab70cebaa2522c
ceejayps <ceejayps1308@gmail.com>
2022-04-23 21:35:53 -0500

    Update auth-state.dart

--- lib/container/auth-state.dart
+++ lib/container/auth-state.dart
@@ -1,8 +1,9 @@
+import 'package:banking_app_2/container/pages/pin.dart';
 import 'package:flutter/material.dart';
 
 class authState extends StatefulWidget {
-  const authState({Key? key}) : super(key: key);
-
+  authState({Key? key}) : super(key: key);
+  int state = 0;
   @override
   State<authState> createState() => _authStateState();
 }
@@ -10,6 +11,6 @@ class authState extends StatefulWidget {
 class _authStateState extends State<authState> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return widget.state == 0 ? pin(title: "title") : Container();
   }
 }
