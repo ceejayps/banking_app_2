32e8e46d62f425cf563af205a9876d55dc573630
ceejayps <ceejayps1308@gmail.com>
2022-04-23 22:00:53 -0500

    Update homepage.dart

--- lib/container/pages/homepage.dart
+++ lib/container/pages/homepage.dart
@@ -10,6 +10,28 @@ class homepage extends StatefulWidget {
 class _homepageState extends State<homepage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+              Color.fromARGB(255, 106, 90, 217),
+              Color.fromARGB(255, 68, 48, 179)
+            ])),
+        child: Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              elevation: 0,
+              // leading: const IconButton(
+              //     onPressed: null,
+              //     icon: Icon(
+              //       Icons.arrow_back,
+              //       color: Colors.white,
+              //     )),
+              backgroundColor: Colors.transparent,
+              title: const Text(''),
+            ),
+            body: Center()));
   }
 }
