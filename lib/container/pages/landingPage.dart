78e282463835203388fc613a9423135ceb7e25c2
ceejayps <ceejayps1308@gmail.com>
2022-04-24 20:16:40 -0500

    Update landingPage.dart

--- lib/container/pages/landingPage.dart
+++ lib/container/pages/landingPage.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:slider_button/slider_button.dart';
@@ -8,15 +10,41 @@ class landingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomCenter,
-              colors: [
-            Color.fromARGB(255, 249, 250, 254),
-            Color.fromARGB(255, 180, 201, 221)
-          ])),
-      child: Stack(children: [Column()]),
-    );
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomCenter,
+                colors: [
+              Color.fromARGB(255, 249, 250, 254),
+              Color.fromARGB(255, 180, 201, 221)
+            ])),
+        child: Scaffold(
+            appBar: AppBar(),
+            backgroundColor: Colors.black,
+            body: Column(
+              children: [
+                SliderButton(
+                  action: () {
+                    ///Do something here
+                  },
+                  label: Text(
+                    'Slide to cancel Event',
+                    style: TextStyle(
+                        color: Color(0xff4a4a4a),
+                        fontWeight: FontWeight.w500,
+                        fontSize: 17),
+                  ),
+                  icon: Text(
+                    'x',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 40,
+                    ),
+                  ),
+                  buttonColor: Colors.black,
+                ),
+              ],
+            )));
   }
 }
