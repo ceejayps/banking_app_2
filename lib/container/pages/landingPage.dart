4b89b749b7a7d0c0dc6b00a1e35e34104eeb791a
ceejayps <ceejayps1308@gmail.com>
2022-04-24 19:09:36 -0500

    Update landingPage.dart

---
+++ lib/container/pages/landingPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class landingPage extends StatelessWidget {
+  const landingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(children: []),
+    );
+  }
+}
