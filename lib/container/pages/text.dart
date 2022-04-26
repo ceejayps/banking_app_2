5c897300e4da04af16ecee65260cd40420039664
ceejayps <ceejayps1308@gmail.com>
2022-04-25 20:56:14 -0500

    Update text.dart

--- lib/container/pages/text.dart
+++ lib/container/pages/text.dart
@@ -8,23 +8,67 @@ class bankingtest extends StatefulWidget {
 }
 
 class _bankingtestState extends State<bankingtest> {
+  Color dark = Colors.black;
+  Color light = Colors.white;
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
+      color: light,
       child: Stack(
         children: [
           Column(
             children: [
               Expanded(
-                  flex: 2,
-                  child: Container(
-                    color: Colors.white,
+                  flex: 25,
+                  child: Row(
+                    children: [
+                      Container(
+                        color: light,
+                        height: double.infinity,
+                        width: MediaQuery.of(context).size.width / 2,
+                      ),
+                      Container(
+                        color: Colors.black,
+                        height: double.infinity,
+                        width: MediaQuery.of(context).size.width / 2,
+                      ),
+                    ],
                   )),
               Expanded(
-                  flex: 1,
+                  flex: 10,
+                  child: Row(
+                    children: [
+                      Container(
+                        color: light,
+                        height: double.infinity,
+                        width: MediaQuery.of(context).size.width / 2,
+                      ),
+                      Container(
+                        color: Colors.black,
+                        height: double.infinity,
+                        width: MediaQuery.of(context).size.width / 2,
+                      )
+                    ],
+                  ))
+            ],
+          ),
+          Column(
+            children: [
+              Expanded(
+                  flex: 25,
                   child: Container(
-                    color: Colors.black,
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius:
+                            BorderRadius.only(bottomLeft: Radius.circular(60))),
+                  )),
+              Expanded(
+                  flex: 10,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: light,
+                        borderRadius:
+                            BorderRadius.only(topRight: Radius.circular(60))),
                   ))
             ],
           )
